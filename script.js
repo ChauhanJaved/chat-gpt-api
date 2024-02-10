d5f7bdeaec05634e2cc28051c8360bd2b8f3e481
Javed Chauhan <chauhanjaved@gmail.com>
2024-02-10 11:14:36 +0530

    Add: user input req res

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-import { config } from 'dotenv';
-config();
-console.log(process.env.API_KEY)
+import { config } from "dotenv"
+config()
+import { OpenAI } from 'openai'
+import readline from "readline"
+
+const client  = new OpenAI({
+  apiKey: process.env.API_KEY,
+});
+
+const userInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.output,
+})
+
+userInterface.prompt()
+userInterface.on("line", async input => {
+  try {
+    const completion = await client.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: input }],
+    })
+    console.log(completion.choices[0].message)
+    userInterface.prompt()  
+  } catch (error) {
+    console.error
+  }  
+})
+
+
+
